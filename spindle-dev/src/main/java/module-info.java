6ef123cd1ff9a40a7f3f3e3792f3c93f05c651d6
Juuz <6596629+Juuxel@users.noreply.github.com>
2023-04-03 19:28:45 +0300

    dev: Require jdk.zipfs

--- spindle-dev/src/main/java/module-info.java
+++ spindle-dev/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module io.github.juuxel.spindle.dev {
     requires cpw.mods.modlauncher;
     requires com.google.gson;
+    requires jdk.zipfs;
 
     provides cpw.mods.modlauncher.serviceapi.ITransformerDiscoveryService
         with juuxel.spindle.dev.SpindleDevDiscoveryService;
