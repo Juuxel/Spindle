9256a1009a5d861e15ca027950173b909942a994
Juuz <6596629+Juuxel@users.noreply.github.com>
2023-04-03 19:40:17 +0300

    dev: Remove dev discovery service
    Putting everything on the bootstrap classpath works too.

--- spindle-dev/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module io.github.juuxel.spindle.dev {
-    requires cpw.mods.modlauncher;
-    requires com.google.gson;
-
-    provides cpw.mods.modlauncher.serviceapi.ITransformerDiscoveryService
-        with juuxel.spindle.dev.SpindleDevDiscoveryService;
-}
