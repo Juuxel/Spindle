0e9c7648bdb8416ecc751207f279b5b2c1d1a266
Juuz <6596629+Juuxel@users.noreply.github.com>
2023-04-04 23:46:36 +0300

    Add license

--- spindle-bootstrap/src/main/java/module-info.java
+++ spindle-bootstrap/src/main/java/module-info.java
@@ -1,3 +1,9 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 module io.github.juuxel.spindle.bootstrap {
     requires cpw.mods.modlauncher;
     provides cpw.mods.modlauncher.serviceapi.ITransformerDiscoveryService
