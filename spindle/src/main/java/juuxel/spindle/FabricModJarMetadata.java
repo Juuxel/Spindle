ae8352cce232c8950b2729122f72fc83bf6d8fec
Juuz <6596629+Juuxel@users.noreply.github.com>
2023-04-25 22:05:33 +0300

    FabricModJarMetadata: Support custom value and Automatic-Module-Name for module name

--- spindle/src/main/java/juuxel/spindle/FabricModJarMetadata.java
+++ spindle/src/main/java/juuxel/spindle/FabricModJarMetadata.java
@@ -11,10 +11,14 @@ import cpw.mods.jarhandling.SecureJar;
 import juuxel.spindle.util.Logging;
 import juuxel.spindle.util.ModuleNames;
 import net.fabricmc.loader.api.ModContainer;
+import org.jetbrains.annotations.Nullable;
 
 import java.lang.module.ModuleDescriptor;
+import java.util.jar.Attributes;
 
 final class FabricModJarMetadata implements JarMetadata {
+    private static final String CUSTOM_MODULE_NAME_KEY = "spindle:module_name";
+
     private final ModContainer modContainer;
     private final SecureJar jar;
     private String name;
@@ -30,9 +34,32 @@ final class FabricModJarMetadata implements JarMetadata {
         if (name != null) return name;
 
         String id = modContainer.getMetadata().getId();
+
+        // 1. Try custom value.
+        @Nullable var customValue = modContainer.getMetadata().getCustomValue(CUSTOM_MODULE_NAME_KEY);
+        if (customValue != null) {
+            name = customValue.getAsString();
+            Logging.LOGGER.debug(Logging.MODULES, "Using module name {} from custom value for mod {}", name, id);
+            return name;
+        }
+
+        // 2. Try manually specified automatic module name.
+        @Nullable var automaticName = getAutomaticModuleName(jar);
+        if (automaticName != null) {
+            name = automaticName;
+            Logging.LOGGER.debug(Logging.MODULES, "Using automatic module name {} for mod {}", name, id);
+            return name;
+        }
+
+        // 3. Convert mod ID into module name.
         name = ModuleNames.fromModId(id);
         Logging.LOGGER.debug(Logging.MODULES, "Converted mod ID {} into module name {}", id, name);
         return name;
+    }
+
+    private static @Nullable String getAutomaticModuleName(SecureJar jar) {
+        Attributes attributes = jar.moduleDataProvider().getManifest().getMainAttributes();
+        return attributes.getValue("Automatic-Module-Name");
     }
 
     @Override
